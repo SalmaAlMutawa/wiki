44ddb799247c4ee3f6826bb5ca490fc667e38ba5
SalmaAlMutawa <s.almutawa96@gmail.com>
2018-10-08 16:22:48 +0300

    mini project submission

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Page
 
 # Create your views here.
+def page_list(request):
+	items = Page.objects.all()
+	context = {
+		"page_list" : items,
+	}
+	return render(request, 'list.html', context)
+
+def page_detail(request, page_id):
+	specific = Page.objects.get(id=page_id)
+	context = {
+		"page_details": specific,
+	}
+	return render(request, 'detail.html', context)
